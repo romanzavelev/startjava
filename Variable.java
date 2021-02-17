c77c90e559282c414e05b04ed9bf1e9841610788
Roman Zavelev <rzav@mail.ru>
2021-02-17 21:06:06 +0300

    Variable

---
+++ Variable.java
@@ -0,0 +1,18 @@
+public class Variable {   
+    public static void main(String[] args) {
+        byte numberOfCores = 4;
+        System.out.println("numberOfCores = " + numberOfCores);
+        short ramSize = 6;
+        System.out.println("ramSize = " + ramSize);
+        long hddSize = 120;
+        System.out.println("hddSize = " + hddSize);
+        float screenSize = 24;
+        System.out.println("screenSize = " + screenSize);
+        double biosVersion = 1.23;
+        System.out.println("biosVersion = " + biosVersion);
+        char cpuModel = 'p';
+        System.out.println("cpuModel = " + cpuModel);
+        boolean osIsMac = false;
+        System.out.println("osIsMac = " + osIsMac);
+    }
+}
