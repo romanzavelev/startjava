d63a1f5644014f42d1a42fd2f1c35ca109ce721f
Roman Zavelev <rzav@mail.ru>
2021-02-18 10:09:20 +0300

    Variable

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,18 +1,18 @@
 public class Variable {   
     public static void main(String[] args) {
         byte numberOfCores = 4;
-        System.out.println("numberOfCores = " + numberOfCores);
         short ramSize = 6;
-        System.out.println("ramSize = " + ramSize);
         long hddSize = 120;
-        System.out.println("hddSize = " + hddSize);
         float screenSize = 24;
-        System.out.println("screenSize = " + screenSize);
         double biosVersion = 1.23;
-        System.out.println("biosVersion = " + biosVersion);
         char cpuModel = 'p';
-        System.out.println("cpuModel = " + cpuModel);
         boolean osIsMac = false;
+        System.out.println("cpuModel = " + cpuModel);
+        System.out.println("biosVersion = " + biosVersion);
+        System.out.println("screenSize = " + screenSize);
+        System.out.println("hddSize = " + hddSize);
+        System.out.println("ramSize = " + ramSize);
+        System.out.println("numberOfCores = " + numberOfCores);
         System.out.println("osIsMac = " + osIsMac);
     }
 }
