0a4ae9152f86009fcc39323c36fcde675c3445e6
Roman Zavelev <rzav@mail.ru>
2021-02-19 11:38:40 +0300

    Update Cycle.java

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,6 +1,6 @@
 public class Cycle {
     public static void main(String[] args) {
-        for (int i = 0; i <= 20 ; i++ ) {
+        for (int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
 
