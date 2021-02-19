ab10e9aba1d50d791a504e8b2620df9202d7dda2
Zavelev Roman <rzav@mail.ru>
2021-02-19 20:10:56 +0300

    1

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,45 +1,22 @@
 public class Calculator {   
     public static void main(String[] args) {
-        int x;
-        int y;
-        char sign;
-
-        x = 5;
-        y = 6;
-        sign = '+';
+        int x = 5;
+        int y = 6;
+        char sign = '+';
+        
         if (sign == '+') {
             System.out.println(x+y);
-        }
-
-        x = 2;
-        y = 3;
-        sign = '*';
-        if (sign == '*') {
+        } else if (sign == '*') {
             System.out.println(x*y);
-        }
-
-        x = 6;
-        y = 3;
-        sign = '/';
-        if (sign == '/') {
+        } else if (sign == '/') {
             System.out.println(x/y);
-        }
-
-        x = 2;
-        y = 3;
-        sign = '^';
-        if (sign == '^') {
+        } else if (sign == '^') {
             int sum = x;
             for (int i = 2; i <= y; i++) {
                 sum = sum * x;
             }
             System.out.println(sum);
-        }
-
-        x = 10;
-        y = 4;
-        sign = '%';
-        if (sign == '%') {
+        } else if (sign == '%') {
             System.out.println(x%y);
         }  
     }
