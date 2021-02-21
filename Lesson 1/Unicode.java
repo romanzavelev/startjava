edb26455bfd830d93357a992966130aac872490b
Roman Zavelev <rzav@mail.ru>
2021-02-21 20:09:45 +0300

    Unicode

--- Lesson 1/Unicode.java
+++ Lesson 1/Unicode.java
@@ -1,7 +1,7 @@
 public class Unicode {   
     public static void main(String[] args) {
-        for (char symbol  = 33; symbol  <= 126; symbol ++) {
-            System.out.println(symbol );
+        for (char symbol = 33; symbol <= 126; symbol++) {
+            System.out.println(symbol);
         }
     }
 }
