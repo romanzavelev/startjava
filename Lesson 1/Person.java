62e889bed8f701921edf2a558d5dac756e943d48
Roman Zavelev <rzav@mail.ru>
2021-02-22 19:27:06 +0300

    Person

---
+++ Lesson 1/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    char sex = 'w';
+    String name = "Мария";
+    int height = 160;
+    int weight = 45;
+    int age = 25;
+
+    void go() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    void talk() {
+
+    }
+
+    void learnJava() {
+
+    }
+}
