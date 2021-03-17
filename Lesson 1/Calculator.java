95c3a675c9951fc1466f557d7c0ee8fff05c6a99
Zavelev Roman <rzav@mail.ru>
2021-03-17 10:41:49 +0300

    "1

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,23 +1,25 @@
 public class Calculator {   
-    public static void main(String[] args) {
-        int x = 5;
-        int y = 6;
-        char sign = '+';
-
-        if (sign == '+') {
-            System.out.println(x + y);
-        } else if (sign == '*') {
-            System.out.println(x * y);
-        } else if (sign == '/') {
-            System.out.println(x / y);
-        } else if (sign == '^') {
-            int result = x;
+    public math(int x, char sign, int y) {
+        switch (sign) {
+            case ('+'):
+            return (x + y);
+            brake;
+            case ('*'):
+            return (x * y);
+            brake;
+            case ('/'):
+            return (x / y);
+            brake;
+            case ('^'):
+             int result = x;
             for (int i = 2; i <= y; i++) {
                 result *= x;
             }
-            System.out.println(result);
-        } else if (sign == '%') {
-            System.out.println(x % y);
-        }  
+            return result;
+            brake;
+            case ('%'):
+            return (x % y);
+            brake;
+        } 
     }
 }
---
+++ Lesson 1/CalculatorTest.java
@@ -0,0 +1,12 @@
+public class CalculatorTest {   
+    public static void main(String[] args) {
+            int x;
+            int y;
+            char sign;
+            Scanner scan = new Scanner(System.in);
+            Calculator calc = new Calculator();
+            System.out.println("Введите первое число: ");
+            char sign = scan.next();
+            System.out.println(sign);
+    }
+}
