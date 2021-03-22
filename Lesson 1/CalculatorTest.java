a4051b8a472bb1825b0f1f172c4e9911b995edc5
Roman Zavelev <rzav@mail.ru>
2021-03-22 11:24:32 +0300

    2

--- Lesson 1/CalculatorTest.java
+++ Lesson 1/CalculatorTest.java
@@ -3,21 +3,18 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Calculator calc = new Calculator();
-        start:
-        System.out.print("Введите первое число: ");
-        int x = scan.nextInt();
-        System.out.print("Введите знак математической операции: ");
-        char sign = scan.next().charAt(0);
-        System.out.print("Введите второе число: ");
-        int y = scan.nextInt();
-        System.out.println(Calculator.math(x,sign,y));
-        String answer = "";
-        while ((answer != "да") && (answer != "нет")) {
+        String answer = "да";
+        while (answer != "да") {
+            System.out.print("Введите первое число: ");
+            int x = scan.nextInt();
+            System.out.print("Введите знак математической операции: ");
+            char sign = scan.next().charAt(0);
+            System.out.print("Введите второе число: ");
+            int y = scan.nextInt();
+            System.out.println(Calculator.math(x,sign,y));
+            while (answer != "да")
             System.out.print("Хотите продолжить вычисления? [да/нет]:");
             answer = scan.nextLine();
-        }
-        if (answer == "да") {
-            return start;
         }
     }
 }
