fde4a77803486fb32a52cf7dfa401263e15630fe
Zavelev Roman <rzav@mail.ru>
2021-03-22 19:38:24 +0300

    calculator

--- Lesson 1/CalculatorTest.java
+++ Lesson 1/CalculatorTest.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
-public class CalculatorTest {   
+public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Calculator calc = new Calculator();
-        String answer = "да";
-        while (answer != "да") {
+        String answer = "yes";
+        while (answer.equals("yes")) {
             System.out.print("Введите первое число: ");
             int x = scan.nextInt();
             System.out.print("Введите знак математической операции: ");
             char sign = scan.next().charAt(0);
             System.out.print("Введите второе число: ");
             int y = scan.nextInt();
+            scan.nextLine();
             System.out.println(Calculator.math(x,sign,y));
-            while (answer != "да")
-            System.out.print("Хотите продолжить вычисления? [да/нет]:");
-            answer = scan.nextLine();
+            answer = "";
+            while (! answer.equals("yes") && ! answer.equals("no")) {
+                System.out.println("Хотите продолжить вычисления? [yes/no]:");
+                answer = scan.nextLine();
+            }
         }
     }
 }
