3961e491613bb3e695c6e1c680d2982a13660500
Roman Zavelev <rzav@mail.ru>
2021-04-23 19:21:47 +0300

    sql

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,12 @@
+-- create database
+
+CREATE TABLE Jaegers(
+    id SERIAL PRIMARY KEY, 
+    modelName TEXT,
+    mark TEXT, 
+    height INTEGER,
+    weight INTEGER, 
+    status TEXT, 
+    origin TEXT, 
+    launch DATE, 
+    kaijuKill INTEGER);
---
+++ src/com/startjava/lesson_5/queries.sql
@@ -0,0 +1,9 @@
+SELECT * FROM Jaegers;
+SELECT * FROM Jaegers WHERE mark = 'Mark-1' or mark = 'Mark-2';
+SELECT * FROM Jaegers ORDER BY mark DESC;
+SELECT * FROM Jaegers WHERE launch = (SELECT MIN(launch) FROM Jaegers);
+SELECT * FROM Jaegers WHERE kaijuKill = (SELECT MIN(kaijuKill) FROM Jaegers);
+SELECT * FROM Jaegers WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM Jaegers);
+SELECT avg(weight) FROM Jaegers;
+UPDATE Jaegers SET kaijuKill = kaijuKill+1 WHERE STATUS = 'intact';
+DELETE FROM Jaegers WHERE STATUS = 'Destroyed';
